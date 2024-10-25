afeb80bada265be16c0942bc36e05a0c8f691b0f
daffa03x <mobadaff@gmail.com>
2024-10-25 08:34:44 +0700

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
+import searchIcon from "../../assets/search_icon.svg";
+import bellIcon from "../../assets/bell_icon.svg";
+import profileImg from "../../assets/profile_img.png";
+import caretIcon from "../../assets/caret_icon.svg";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="navbar-left">
+        <img src={logo} alt="" />
+        <ul>
+          <li>Home</li>
+          <li>TV Shows</li>
+          <li>Movies</li>
+          <li>New & Popular</li>
+          <li>My List</li>
+          <li>Browse by Languages</li>
+        </ul>
+      </div>
+      <div className="navbar-right">
+        <img src={searchIcon} alt="" className="icons" />
+        <p>Children</p>
+        <img src={bellIcon} alt="" className="icons" />
+        <div className="navbar-profile">
+          <img src={profileImg} alt="" className="profile" />
+          <img src={caretIcon} alt="" className="icons" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
